d276d794c41e582d7a087a63a76d694d20297d4b
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-03-02 12:57:05 +0200

    edit

--- server.js
+++ server.js
@@ -41,6 +41,38 @@ app.post("/newmessage", function (req, res) {
     res.sendFile(__dirname + "/public/continue.html");
 });
 
+app.get("/ajaxmessage", function (req, res) {
+    res.sendFile(__dirname + "/public/ajaxform.html");
+});
+
+// POST-tyyppiseen sivupyyntöön reagoiva reitti
+app.post("/ajaxmessage", function(req, res) {
+    var data = require("./public/data.json");
+    var today = new Date();
+
+    var date = today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+
+    data.push({
+        "Name": req.body.name,
+        "Country": req.body.country,
+        "Message": req.body.message,
+        "Date": date
+    });
+
+    var jsonStr = JSON.stringify(data);
+
+    fs.writeFile("public/data.json", jsonStr, (err) => {
+        if (err) throw err;
+        console.log("It is saved!");
+    });
+
+    var name = req.body.name;
+    var country =  req.body.country;
+    var message =  req.body.message;
+
+  res.send("You send an AJAX message: " + message + ". You can now see it at the Guestbook!");
+});
+
 
 app.get("/guestbook", function (req, res) {
     res.sendFile(__dirname + "/public/guestbook.html");
