bc2e24cd97bf288dc67e995fc5c9fb8b99c5e355
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-03-02 15:21:07 +0200

    Adding comments

--- public/ajax.js
+++ public/ajax.js
@@ -1,17 +1,15 @@
-// Varmistetaan, että sivu on ladattu kokonaan, ennenkuin tehdään mitään
+// Varmistetaan, että sivu on ladattu kokonaan, ennenkuin tehdään mitään.
 $(document).ready(function () {
 
-    // Etsitään haluttu nappi ja lisätään kuuntelija
+    // Etsitään haluttu nappi ja lisätään kuuntelija.
     $("#ajaxbutton").click(function () {
-
-        // Tallennetaan lomakkeen kentät data-muuttujaan
+        // Tallennetaan lomakkeen kentät data-muuttujaan.
         var data = {
             name: $("#name2").val(),
             country: $("#country2").val(),
             message: $("#message2").val()
         };
-
-        // Jos kentät ovat tyhjät, annetaan errorviesti
+        // Jos kentät ovat tyhjät, annetaan errorviesti.
         if ($("#name2").val() == "") {
             alert("One or more fields are empty! Check that you have written something on every field.");
         } else if ($("#country2").val() == "") {
@@ -19,14 +17,11 @@ $(document).ready(function () {
         } else if ($("#message2").val() == "") {
             alert("One or more fields are empty! Check that you have written something on every field.");
         } else {
-
-
-            // Lähetetään AJAX olio joka palauttaa vastauksensa status-kenttään   
+            // Lähetetään AJAX olio joka palauttaa vastauksensa status-kenttään.   
             $.post("/ajaxmessage", data, function (response, status) {
                 $("#status").html(response);
             });
-
         }
-
     });
+    
 });
--- public/book.js
+++ public/book.js
@@ -1,9 +1,9 @@
+// Varmistetaan, että sivu on ladattu kokonaan, ennenkuin tehdään mitään.
 $(document).ready(function () {
-
+    // Haetaan dataa JSON tiedostosta
     $.getJSON("data.json", function (data) {
-
-
-       var table = "<table><tr><th>Name</th><th>Country</th><th>Message</th><th>Date</th></tr>";
+        // Luodaan taulukko ja lisätään siihen JSON tiedostosta halutut tiedot.
+        var table = "<table><tr><th>Name</th><th>Country</th><th>Message</th><th>Date</th></tr>";
         for (var i = 0; i < data.length; i++) {
             table +=
                 "<tr>" +
@@ -13,7 +13,7 @@ $(document).ready(function () {
                 "<td>" + data[i].Date + "</td>" +
                 "</tr>";
         }
-
+        // Asetetaan luotu taulukko place-elementtiin.
         $("#place").html(table);
     });
 });
